3e5f5f2e2d4bb3fa4a0a335dc37ad76989c4b9bb
Sanchala O.P.R <rspathirana1017@gmail.com>
2025-04-04 17:42:40 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,21 @@
+
+function contactMe() {
+    
+}
+
+function downloadCV() {
+    alert("Downloading CV...");
+}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {  
+    anchor.addEventListener('click', function (e) {  
+        e.preventDefault();  
+
+        const target = document.querySelector(this.getAttribute('href'));  
+        if (target) {  
+            target.scrollIntoView({  
+                behavior: 'smooth',  
+                block: 'start'  
+            });  
+        }  
+    });  
+});  
